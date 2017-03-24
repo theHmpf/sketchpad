78a52f01ad3e7f6999050a14110e495275a0bddd
Timo Schlüschen <d2-d4@t-online.de>
2017-03-24 18:08:08 +0100

    Project finished

--- script.js
+++ script.js
@@ -4,48 +4,64 @@ $.when( $.ready ).then(function(){
 
   var grid = $('#grid-container');
 
-  var gridNumber = 10000;
+  //var tailLength = 1000;
+  var resetButton = $('#reset-button');
 
-  var tailLength = 1000;
-  // Create as much divs as gridNumber
-  for (var i = 0; i < gridNumber; i++) {
-      var divNew = $("<div id=grid-div-" + i + "></div>").addClass("grid-div");
-      grid.append(divNew);
-  }
+  // Remove old divs and then creates as much divs as gridNumber
+  function createDivs(number){
 
-  var gridDivs = $('.grid-div');
+    grid.empty();
 
-  // Place always the last hovered div at the end of this array
-  var activeDivArray = [];
-
-  function divHoverIn(div){
-    div.css({
-      "background-color": "rgb(130, 51, 98)"
-    });
-
-    activeDivArray.push(div);
-
-  }
-
-  function divHoverOut(div) {
-
-    if(activeDivArray.length >= tailLength) {
-      console.log(activeDivArray);
-
-      activeDivArray[0].css({
-        "background-color": "rgb(65, 156, 130)"
-      });
-
-      activeDivArray.shift();
-      console.log(activeDivArray);
+    for (var i = 0; i < number; i++) {
+        var divNew = $("<div id=grid-div-" + i + "></div>").addClass("grid-div");
+        grid.append(divNew);
     }
 
+    var gridDivs = $('.grid-div');
+    // Place always the last hovered div at the end of this array
+    //var activeDivArray = [];
+
+    function divHoverIn(div){
+      div.css({
+        "background-color": "rgb(130, 51, 98)"
+      });
+
+      //activeDivArray.push(div);
+
+    }
+
+    function divHoverOut(div) {
+
+      // if(activeDivArray.length >= tailLength) {
+      //   console.log(activeDivArray);
+      //
+      //   activeDivArray[0].css({
+      //     "background-color": "rgb(65, 156, 130)"
+      //   });
+      //
+      //   activeDivArray.shift();
+      //   console.log(activeDivArray);
+      // }
+
+    }
+
+    gridDivs.hover(function() {
+      divHoverIn( $( this ) );
+    }, function() {
+      divHoverOut( $( this ) );
+    });
   }
 
-  gridDivs.hover(function() {
-    divHoverIn( $( this ) );
-  }, function() {
-    divHoverOut( $( this ) );
+  createDivs(50000);
+
+  resetButton.click(function(){
+    gridNumber = prompt("How much grid segments do you want?");
+    createDivs(gridNumber);
+
+    var gridDivs = $('.grid-div');
+    gridDivs.css({
+      "background-color": "rgb(65, 156, 130)"
+    });
   });
 
 });
